8d5098ca6a2864bdf85e724dfe23030098151f83
hehe876 <ndao3@asu.edu>
2016-11-18 10:52:42 -0700

    Added Library project with example class

---
+++ ourLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ourLibrary
+{
+    /* Example class
+     public class myMath 
+     { 
+		public static long abs (long x) 
+        { 
+			if (x >= 0) return (x); else return (-x); 
+		} 
+	 } 
+     */
+    public class Class1
+    {
+    }
+}
