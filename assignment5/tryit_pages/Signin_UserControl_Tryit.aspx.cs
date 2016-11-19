51c9e36c4786c328157e7b5a0ca85cc3ccb81959
Kyle Ferguson <hanbrolo117@gmail.com>
2016-11-18 21:10:44 -0700

    version update

---
+++ assignment5/tryit_pages/Signin_UserControl_Tryit.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace assignment5
+{
+    public partial class Signin_UserControl_Tryit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void LoginHandler(string username, string password, EventArgs e)
+        {
+            this.lor.Text = string.Format("Logged in! username: {0} | password: {1}", username, password);
+        }
+
+        public void RegisterHandler(string username, string password, EventArgs e)
+        {
+            this.lor.Text = string.Format("Registering! username: {0} | password: {1}", username, password);
+        }
+    }
+}
